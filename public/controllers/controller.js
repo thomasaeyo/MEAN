3a9b43288da9452655a0e1db30b682170c271d67
ttl33 <ttl33@cornell.edu>
2015-04-03 16:57:59 -0400

    first commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,18 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log("Hello from controller");
+	$scope.people = [];	
+
+	$scope.search = function() {
+		console.log($scope.query);
+		$http.post('/search', {query:$scope.query}).success(function(response) {
+			console.log(response);
+			$scope.people = response;
+		});
+	};
+	// $http.get('/search').success(function(response) {
+	// 	console.log('I got the data I requested');
+	// 	$scope.personlist = response;	
+	// });
+}]);
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('people',['people']);
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.post('/search', function (req, res) {
+	db.people.find(function (err, docs) {
+		console.log(docs);
+	});
+	console.log("I received a POST request")
+	console.log(req.body['query']);
+	db.people.find({'keywords': req.body['query']}, function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
